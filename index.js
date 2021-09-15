8d98fdfa554cb46084fbeeaa82b7273efd65553b
Roger Perez <rogelio_perez_98@hotmail.com>
2021-09-15 14:56:01 -0500

    Creation of the App's menu

---
+++ index.js
@@ -0,0 +1,45 @@
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Search = require("./models/Search");
+
+
+const main = async () => {
+    let opcionMenu;
+    const search = new Search();
+    do {
+        opcionMenu = await inquirerMenu();
+        switch (opcionMenu) {
+            case 1:
+            //Show message to the user 
+            const place = await leerInput('Enter the name of the place to search:')
+            // Search the differents places
+
+            // Select the correct place
+
+            // Show the weater
+
+            // Show general information about the place
+            console.log('\n Information of the place \n'.green);
+            console.log('City: ', place);
+            console.log('Latitude: ',);
+            console.log('Longitude: ',);
+            console.log('Temperature: ',)
+            console.log('Min: ',);
+            console.log('Max: ',);
+            break;
+            case 2:
+                console.log('opt 2');
+            break;
+            case 3:
+                console.log('Adioss');
+            break;
+                    
+            default:
+            break;
+
+        }    
+        if(opcionMenu !== 3) await pausa();
+    } while (opcionMenu !== 3);
+    
+}
+
+main();
---
+++ models/Search.js
@@ -0,0 +1,19 @@
+
+class Search{
+
+    history = []
+    constructor(){
+        // Read if exist some records in the database
+    }
+
+    async city( place = ''){
+        // request http to the api
+        console.log(place);
+
+        // return all the places that it match wiht the search
+        return [];
+    }
+
+}
+
+module.exports = Search;
